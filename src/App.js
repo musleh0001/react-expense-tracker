a8d9a669e26fb363a1798fa220293e58917a0f43
musleh0001 <khan.muslehuddin0001@gmail.com>
2022-04-02 12:33:29 +0600

    add UI components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-function App() {
+import React from "react";
+
+import "./app.css";
+import Card from "./components/UI/Card";
+import ExpenseItem from "./components/Expenses/ExpenseItem";
+
+const App = () => {
+	const expenses = [
+		{
+			id: "e1",
+			title: "Toilet Paper",
+			amount: 94.12,
+			date: new Date(2020, 7, 14),
+		},
+		{ id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+		{
+			id: "e3",
+			title: "Car Insurance",
+			amount: 294.67,
+			date: new Date(2021, 2, 28),
+		},
+		{
+			id: "e4",
+			title: "New Desk (Wooden)",
+			amount: 450,
+			date: new Date(2021, 5, 12),
+		},
+	];
+
 	return (
 		<div>
 			<h2>Let's get started!</h2>
-			<p>This is also visible!</p>
+			<Card className="expenses">
+				{expenses.map((expense) => (
+					<ExpenseItem key={expense.id} {...expense} />
+				))}
+			</Card>
 		</div>
 	);
-}
+};
 
 export default App;
---
+++ src/components/Expenses/ExpenseItem.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+
+const ExpenseItem = ({ title, amount, date }) => {
+	const [expenseTitle, setExpenseTitle] = useState(title);
+
+	const clickHandler = () => {
+		setExpenseTitle("Updated");
+	};
+
+	return (
+		<Card className="expense-item">
+			<ExpenseDate date={date} />
+			<div className="expense-item__description">
+				<h2>{expenseTitle}</h2>
+				<div className="expense-item__price">$ {amount}</div>
+			</div>
+			<button onClick={clickHandler}>Change Title</button>
+		</Card>
+	);
+};
+
+export default ExpenseItem;
